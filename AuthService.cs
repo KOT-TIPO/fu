021eb7aaa47d968b79ee3834624fb79507037eb0
KOT-TIPO <t143alena@gmail.com>
2025-04-14 14:37:07 +0300

    Add files via upload

---
+++ AuthService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yagnov.BDModel;
+using Yagnov.Model;
+using System.Data.Entity;
+
+namespace Yagnov.Services
+{
+    public class AuthService 
+    {
+        ShopDBEntities dbContext = new ShopDBEntities();
+
+        public bool CheckData(string login, string pass)
+        {
+            var user = dbContext.Users.Include(u => u.Role).FirstOrDefault(u => u.Login == login && u.Pass == pass);
+
+            if (user != null && user.Pass == pass)
+            {
+                MainWindow Window = new MainWindow(user);
+                Window.Show();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
