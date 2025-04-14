ed62d16e24890602a84aa2753db0e8d333f3fbaa
KOT-TIPO <t143alena@gmail.com>
2025-04-14 14:35:56 +0300

    Add files via upload

---
+++ AddOrEditWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Yagnov.BDModel;
+
+namespace Yagnov
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrEditWindow.xaml
+    /// </summary>
+    public partial class AddOrEditWindow 
+    {
+
+        ShopDBEntities _dbContext = new ShopDBEntities();
+        private Products _product;
+        public bool DialogResult { get; private set; }
+        public AddOrEditWindow(Products selectedProduct, ShopDBEntities shopDB)
+        {
+            InitializeComponent();
+            var _dbContext1 = _dbContext;
+            if (_product != null)
+            {
+                NameTextBox.Text = _product.ProductName;
+                PriceTextBox.Text = _product.Price.ToString();
+                DescriptionTextBox.Text = _product.Description;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(NameTextBox.Text))
+                {
+                    MessageBox.Show("Введите название товара");
+                    return;
+                }
+                if (!decimal.TryParse(PriceTextBox.Text, out decimal price))
+                {
+                    MessageBox.Show("Введите корректную цену товара.");
+                    return;
+                }
+                if (_product == null)
+                {
+                    _product = new Products
+
+                    {
+                        ProductName = NameTextBox.Text,
+                        price = price,
+                        Description = DescriptionTextBox.Text
+                    };
+                    _dbContext.Products.Add(_product);
+                }
+                else
+                {
+                    _product.ProductName = NameTextBox.Text;
+                    _product.Price = price;
+                    _product.Description = DescriptionTextBox.Text;
+                }
+                _dbContext.SaveChanges();
+                DialogResult = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении товара:{ex.Message}");
+        }
+    }
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Yagnov.BDModel;
+
+namespace Yagnov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        ShopDBEntities dbContext = new ShopDBEntities();
+
+        public object ProductsDataGrid { get; private set; }
+        private Users currentUser;
+        public MainWindow(Users currentUser)
+        {
+            InitializeComponent();
+            this.currentUser = currentUser;
+            MessageBox.Show(currentUser.Role.Role_Name);
+        }
+
+
+
+        private void LoadData()
+        {
+
+            try
+            {
+                var products = dbContext.Products.ToList();
+                ProductDataGrid.ItemsSource = products;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки данных: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedProduct = ProductDataGrid.SelectedItem as Products;
+            if (selectedProduct == null)
+            {
+                MessageBox.Show("Выберете товар для редактирования.");
+                return;
+            }
+            AddOrEditWindow editWindow = new AddOrEditWindow(selectedProduct, dbContext);
+            if (editWindow.ShowDialog() == true)
+            {
+                LoadData();
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            AddOrEditWindow editWindow = new AddOrEditWindow(null, dbContext);
+            if (editWindow.ShowDialog() == true)
+            {
+                LoadData();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProductDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
+
+      
